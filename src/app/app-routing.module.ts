4cfaf08cead02214c1480b197692c084f81e3dcb
Asmaa MELHAOUI <asmaa.melhaoui@ibm.com>
2022-09-06 23:13:32 +0100

    with ngrx

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'productList', component: ProductListComponent },
+  { path: 'productDetails/:id', component: ProductDetailsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
